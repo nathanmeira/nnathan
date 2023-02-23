5707f294bb5a405c56b2379b9e7b73208ccede6e
nathanmeira <nathan.meira1@gmail.com>
2023-02-22 22:54:10 -0700

    wip: timeline

---
+++ components/Event.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="event" class="mb-8 flex justify-between items-center w-full"
+  :class='alignRight ? "right-timeline" : "flex-row-reverse left-timeline"'
+  >
+    <div class="order-1 w-5/12"></div>
+    <div class="order-1 w-5/12 px-1 py-4"
+      :class='alignRight ? "text-left" : "text-right"'
+    >
+      <p class="mb-3 text-base text-yellow-300">{{ date }}</p>
+      <h4 class="mb-3 font-bold text-lg md:text-2xl">{{ title }}</h4>
+      <p class="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
+        {{ description }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'Event Title',
+    },
+    date: {
+      type: String,
+      default: 'Date',
+    },
+    description: {
+      type: String,
+      default: 'Description',
+    },
+    alignRight: {
+      type: Boolean,
+      default: false,
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
