86ab5e61618b7fbbf91342b77677809e32ada0c4
nathanmeira <nathan.meira1@gmail.com>
2023-02-24 12:07:19 -0700

    wip: Portfolio page

--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -1,14 +1,52 @@
+
 <template>
   <PageWrap>
-    My Work
+      <div class="mx-auto max-w-2xl py-8 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div class="text-center">
+          <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Portfolio</h2>
+          <p class="mt-3 max-w-2xl mx-auto text-xl text-white sm:mt-4">
+            Here are some of my projects.
+          </p>
+        </div>
+        <div class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+          <div v-for='work in works' :key='work.id'>
+            <div class="flex flex-col rounded-lg shadow-lg overflow-hidden transform transition-all hover:scale-105">
+              <div class="flex-shrink-0">
+                <img class="h-48 w-full object-cover" :src="work.images[0]" alt="">
+              </div>
+              <div class="flex-1 bg-white p-6 flex flex-col justify-between">
+                <div class="flex-1">
+                  <p class="text-sm font-medium" style='color: #2a9d8f'>
+                    {{ work.category }}
+                  </p>
+                  <p class="text-xl font-semibold text-gray-900">
+                    {{ work.name }}
+                  </p>
+                  <p class="mt-3 text-base text-gray-500">
+                    {{ work.description }}
+                  </p>
+                  <router-link :to="`/portfolio/work/${work.slug}`">
+                    <div class='mt-4 py-2 px-2 border text-center text-white border-outline-green rounded-md' style='background-color: #2a9d8f'>View more</div>
+                  </router-link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
   </PageWrap>
+
 </template>
 
 <script>
+import work from '~/mock/work.json';
+
 export default {
+  data() {
+    return {
+      works: work,
+    }
+  }
 }
+
 </script>
-
-<style scoped>
-
-</style>
--- pages/timeline/index.vue
+++ pages/timeline/index.vue
@@ -1,7 +1,7 @@
 <template>
   <PageWrap>
     <section>
-      <div class="bg-black text-white py-8">
+      <div class="text-white py-8">
         <div class="container mx-auto flex flex-col items-start md:flex-row my-12 md:my-24">
           <div class="flex flex-col w-full sticky md:top-36 lg:w-1/3 mt-2 md:mt-12 px-8">
             <p class="text-yellow-300 uppercase tracking-loose vueGreen">Timeline</p>
